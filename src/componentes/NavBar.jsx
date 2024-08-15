4550984f813b87c57bdd3fc09b903d2552edd806
JoseRel8ed <jose@rel8ed.to>
2024-08-14 21:45:22 -0500

    Boton regreso HOME

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,10 +1,17 @@
 import IconHome from "./Iconos/IconHome"
 import IconVidaLlena from "./Iconos/IconVidaLlena"
+import { useNavigate } from "react-router-dom";
+
 
 function NavBar({ nombreJuego, score }) {
+  const navigate = useNavigate();
+
+const handleClick = () => {
+  navigate("/");
+};
   return (
     <nav className='rounded-xl bg-[#D9D9D9] absolute top-0 my-5 w-[80%] h-[70px]  opacity-70 flex items-center justify-between px-4'>
-      <button className="flex items-center justify-center bg-[#FFDD33] rounded-full p-3 shadow-normal" tabIndex={0} aria-label="Regresar al menu principal">
+      <button className="flex items-center justify-center bg-[#FFDD33] rounded-full p-3 shadow-normal" tabIndex={0} aria-label="Regresar al menu principal" onClick={handleClick}>
         <IconHome width={28} height={28} className='cursor-pointer' />
       </button>
       <p className='text-2xl font-bold ml-2' tabIndex={0}>Juego de: {nombreJuego}</p>
